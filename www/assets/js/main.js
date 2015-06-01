cad0e928581453b5c208d69bb39075141df2bee0
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2015-06-01 09:51:17 +0200

    feat(poker): initial version of poker application

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,99 +1,95 @@
 "use strict";
+
 // initialize Hoodie
 var hoodie  = new Hoodie();
 
-// Todos Collection/View
-function Todos($element) {
-  var collection = [];
-  var $el = $element;
+// React
 
-  // Handle marking todo as "done"
-  $el.on('click', 'input[type=checkbox]', function() {
-    hoodie.store.remove('todo', $(this).parent().data('id'));
-    return false;
-  });
+hoodie.store.clear().then( function() {
+  hoodie.store.add('estimates', {id: 'myself', value: 2});
+  hoodie.store.add('estimates', {id: 'alexis', value: 5});
+  hoodie.store.add('estimates', {id: 'bro', value: 3});
+});
 
-  // Handle "inline editing" of a todo.
-  $el.on('click', 'label', function() {
-    $(this).parent().parent().find('.editing').removeClass('editing');
-    $(this).parent().addClass('editing');
-    return false;
-  });
+var Card = React.createClass({
+  propTypes: {
+    value: React.PropTypes.number
+  },
 
-  // Handle updating of an "inline edited" todo.
-  $el.on('keypress', 'input[type=text]', function(event) {
-    if (event.keyCode === 13) {
-      hoodie.store.update('todo', $(this).parent().data('id'), {title: event.target.value});
-    }
-  });
+  handleClick: function(){
+    hoodie.store.updateOrAdd('estimates', 'myself', {value: this.props.value})
+  },
 
-  // Find index/position of a todo in collection.
-  function getTodoItemIndexById(id) {
-    for (var i = 0, len = collection.length; i < len; i++) {
-      if (collection[i].id === id) {
-        return i;
-      }
-    }
-    return null;
+  render: function(){
+    return <div className="card" onClick={this.handleClick}>{this.props.value}</div>
   }
+});
 
-  function paint() {
-    $el.html('');
-    collection.sort(function(a, b) {
-      return ( a.createdAt > b.createdAt ) ? 1 : -1;
+var User = React.createClass({
+  render: function(){
+    return <div className="card-name">{this.props.name}</div>
+  }
+});
+
+var PlayedCard = React.createClass({
+  propTypes: {
+    value: React.PropTypes.number,
+    name: React.PropTypes.string
+  },
+
+  render: function(){
+    return <div className="card-played">
+      <Card value={this.props.value} />
+      <User name={this.props.name} />
+    </div>
+  }
+});
+
+var Deck = React.createClass({
+  render: function(){
+    var cards = [1, 2, 3, 4, 5, 8, 13, 21].map(function(cardValue){
+      return <Card value={cardValue} key={cardValue} />
+    }.bind(this));
+
+    return <div className="deck">{cards}</div>
+  }
+});
+
+var Table = React.createClass({
+  propTypes: {
+    estimates: React.PropTypes.array
+  },
+
+  render: function(){
+    var playedCards = this.props.estimates.map(function(playedCard){
+      return <PlayedCard value={playedCard.value} name={playedCard.id} />
     });
-    for (var i = 0, len = collection.length; i<len; i++) {
-      $el.append(
-        '<li data-id="' + collection[i].id + '">' +
-          '<input type="checkbox"> <label>' + collection[i].title + '</label>' +
-          '<input type="text" value="' + collection[i].title + '"/>' +
-        '</li>'
-      );
+
+    return <div className="table">{playedCards}</div>
+  }
+});
+
+var PokerApp = React.createClass({
+  getInitialState: function(){
+    return {
+      estimates: []
     }
-  }
+  },
 
-  this.add = function(todo) {
-    collection.push(todo);
-    paint();
-  };
+  componentDidMount: function(){
+    hoodie.store.on('change', function() {
+      hoodie.store.findAll('estimates').then(function(allEstimates) {
+        this.setState({estimates: allEstimates});
+      }.bind(this));
+    }.bind(this));
+  },
 
-  this.update = function(todo) {
-    collection[getTodoItemIndexById(todo.id)] = todo;
-    paint();
-  };
-
-  this.remove = function(todo) {
-    collection.splice(getTodoItemIndexById(todo.id), 1);
-    paint();
-  };
-
-  this.clear = function() {
-    collection = [];
-    paint();
-  };
-}
-
-// Instantiate Todos collection & view.
-var todos = new Todos($('#todolist'));
-
-// initial load of all todo items from the store
-hoodie.store.findAll('todo').then(function(allTodos) {
-  allTodos.forEach(todos.add);
-});
-
-// when a todo changes, update the UI.
-hoodie.store.on('todo:add', todos.add);
-hoodie.store.on('todo:update', todos.update);
-hoodie.store.on('todo:remove', todos.remove);
-// clear todos when user logs out,
-hoodie.account.on('signout', todos.clear);
-
-
-// handle creating a new task
-$('#todoinput').on('keypress', function(event) {
-  // ENTER & non-empty.
-  if (event.keyCode === 13 && event.target.value.length) {
-    hoodie.store.add('todo', {title: event.target.value});
-    event.target.value = '';
+  render: function(){
+    return <div>
+      <Table estimates={this.state.estimates} />
+      <Deck />
+    </div>
   }
 });
+
+React.render(<PokerApp />, pokerApp);
